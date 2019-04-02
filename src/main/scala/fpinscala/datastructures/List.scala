45da9424423afb5705db1ad2a9eaeb50498ea00f
h-kanazawa <kanazawahotaka@gmail.com>
2019-04-02 21:58:30 +0100

    3.3 data shareing in functional data structures

---
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -0,0 +1,64 @@
+package fpinscala.datastructures
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  // Exercise 3.2
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => t
+  }
+
+  // Exercise 3.3
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => Cons(h, t)
+  }
+
+  // Exercise 3.4
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n-1)
+    }
+  }
+
+  // Exercise 3.5
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t, f)
+    case _ => l
+  }
+
+  // Exercise 3.6
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("l is empty")
+    case Cons(h, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(tail(List(1,2,3,4,5)))
+    println(setHead(List(1,2,3,4,5), 9))
+    println(drop(List(1,2,3,4,5), 3))
+    println(dropWhile(List(1,2,3,4,5), (x: Int) => x < 3))
+    println(init(List(1,2,3,4,5)))
+  }
+}
