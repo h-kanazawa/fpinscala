2c4a5170c0dc1214eb297d5e41d9bfba18a4e704
h-kanazawa <kanazawahotaka@gmail.com>
2019-04-04 21:22:44 +0100

    3.4 generalize high order functions and recursion

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -54,11 +54,145 @@ object List {
     case Cons(h, t) => Cons(h, init(t))
   }
 
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)(_ + _)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _)
+
+  // Exercise 3.9
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0)((_, x) => x + 1)
+
+  // Exercise 3.10
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+  // Exercise 3.11
+  def sum3(ns: List[Int]) =
+    foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]) =
+    foldLeft(ns, 1.0)(_ * _)
+
+  def length2[A](l: List[A]): Int =
+    foldLeft(l, 0)((len, _) => len + 1)
+
+  // Exercise 3.12
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil: List[A])((acc, a) => Cons(a, acc))
+  // The below code is error. Type of Nil has to be explicitly written.
+  // foldLeft(l, Nil)((newList, h) => Cons(h, newList))
+  // [error]  found   : fpinscala.datastructures.Cons[A]
+  // [error]  required: fpinscala.datastructures.Nil.type
+
+  // Exercise 3.13
+  def foldLeft2[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(l, (b: B) => b)((a, g) => b => g(f(b, a)))(z)
+    // foldRight[A, B => B]
+    // foldRight(List[A], B => B)((A, B => B) => (B => B)): B => B
+    //
+    // The 2nd argument of foldRight
+    // ((a, g) => b => g(f(b, a)))
+    // ((a: A, g: B => B) => (b: B => g(f(b: B, a: A))))
+    //                                  ------------- f(b,a) returns B
+    //                                ---------------- g(f(b,a)) returns B
+
+  def foldRight2[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(l, (b: B) => b)((g, a) => b => g(f(a, b)))(z)
+
+  // Exercise 3.14
+  def append[A](l: List[A], r: List[A]): List[A] =
+    foldRight(l, r)(Cons(_, _))
+
+  // Exercise 3.15
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil: List[A])(append)
+    // foldRight(l, Nil: List[A])(foldRight(l, r)(Cons(_, _)))
+
+  // Exercise 3.16
+  def add1(l: List[Int]): List[Int] =
+    foldRight(l, Nil: List[Int])((h, t) => Cons(h+1, t))
+
+  // Exercise 3.17
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((h, t) => Cons(h.toString, t))
+
+  // Exercise 3.18
+  def map[A, B](as: List[A])(f: A => B): List[B] =
+    foldRight(as, Nil: List[B])((h, t) => Cons(f(h), t))
+
+  // Exercise 3.19
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldRight(as, Nil: List[A])((h, t) => {
+      if (f(h)) Cons(h, t)
+      else t
+    })
+
+  // Exercise 3.20
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    foldRight(as, Nil: List[B])((h, t) => foldRight(f(h), t)(Cons(_, _)))
+    // concat(map(as)(f))
+
+  // Exercise 3.21
+  def filter2[A](as: List[A])(f: A => Boolean): List[A] =
+    flatMap(as)(a => if (f(a))  List(a) else Nil: List[A])
+
+  // Exercise 3.22
+  def zipAdd(as: List[Int], bs: List[Int]): List[Int] = (as, bs) match {
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case (Cons(ah, at), Cons(bh, bt)) => Cons(ah+bh, zipAdd(at, bt))
+  }
+
+  // Exercise 3.23
+  def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = (as, bs) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(ah, at), Cons(bh, bt)) => Cons(f(ah, bh), zipWith(at, bt)(f))
+  }
+
+  // Exercise 3.24
+  @annotation.tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l, prefix) match {
+    case (_, Nil) => true
+    case (Cons(h1, t1), Cons(h2, t2)) if (h1 == h2) => startsWith(t1, t2)
+    case _ => false
+  }
+
+  @annotation.tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => sub == Nil
+    case _ if startsWith(sup, sub) => true
+    case Cons(h, t) => hasSubsequence(t, sub)
+  }
+
   def main(args: Array[String]): Unit = {
     println(tail(List(1,2,3,4,5)))
     println(setHead(List(1,2,3,4,5), 9))
     println(drop(List(1,2,3,4,5), 3))
     println(dropWhile(List(1,2,3,4,5), (x: Int) => x < 3))
     println(init(List(1,2,3,4,5)))
+    println(sum2(List(1,2,3,4,5)))
+    println(product2(List(1,2,3,4,5)))
+    println(length(List(1,2,3,4,5)))
+    println(sum3(List(1,2,3,4,5)))
+    println(product3(List(1,2,3,4,5)))
+    println(reverse(List(1,2,3,4,5)))
+    println(append(List(1,2,3), List(4,5)))
+    println(concat(List(List(1,2,3), List(4,5), List(6,7,8,9))))
+    println(add1(List(1,2,3,4)))
+    println(flatMap(List(1,2,3))(i => List(i,i)))
+    println(filter2(List(1,2,3,4,5))(_ % 2 == 0))
+    println(zipAdd(List(1,4,6), List(2,7,4,21)))
   }
 }
