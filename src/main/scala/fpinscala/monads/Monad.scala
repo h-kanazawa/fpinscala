a213b6f730517b7b3c71a8d7f0585f817bc5c9d3
h-kanazawa <kanazawahotaka@gmail.com>
2019-06-08 21:31:30 +0100

    Add Monad

---
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -0,0 +1,58 @@
+package fpinscala.monads
+
+import fpinscala.state._
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def distribute[A,B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map(fab)(_._1), map(fab)(_._2))
+
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fb) => map(fb)(Right(_))
+  }
+}
+
+object Functor {
+  val listFunctor = new Functor[List] {
+    def map[A,B](as: List[A])(f: A => B): List[B] = as map f
+  }
+}
+
+case class Reader[R, A](run: R => A)
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+
+  def map[A,B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+}
+
+object Monad {
+  val idMonad = new Monad[Id] {
+    override def unit[A](a: => A) = Id(a)
+    override def flatMap[A, B](ida: Id[A])(f: A => Id[B]): Id[B] = ida flatMap f
+  }
+
+  def stateMonad[S] = new Monad[({type lambda[x] = State[S, x]})#lambda] {
+    def unit[A](a: => A): State[S, A] = State(s => (a, s))
+    override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+      st flatMap f
+  }
+
+  def getState[S]: State[S,S] = State(s => (s,s))
+  def setState[S](s: S): State[S,Unit] = State(_ => ((),s))
+
+  def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
+    def unit[A](a: => A): Reader[R, A] = Reader(_ => a)
+    override def flatMap[A,B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] =
+      Reader(r => f(st.run(r)).run(r))
+  }
+}
+
+case class Id[A](value: A) {
+  def map[B](f: A => B): Id[B] = Id(f(value))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+}
